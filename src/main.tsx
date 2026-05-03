f77c9747f1eaff4805d88996aa0a8d785a453a10
Rin <58572875+TurtIeSocks@users.noreply.github.com>
2026-05-03 12:31:26 -0400

    gh pages workflow

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./app.tsx";
 // biome-ignore lint/style/noNonNullAssertion: known to be there
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename={import.meta.env.BASE_URL}>
 			<App />
 		</BrowserRouter>
 	</StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: "/pokemon-tcg-viewer/",
 	plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
